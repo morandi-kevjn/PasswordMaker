8e6b1628b6ea9b29f1d9facecd92b3bd473bdf17
morandi-kevjn <85896336+morandi-kevjn@users.noreply.github.com>
2024-04-25 18:49:37 +0200

    A simple program to create a password

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation + '!@#$%^&*()_+-=[]{}|;:,.<>?'
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+
+def main():
+    length = int(input("Enter the length of the password: "))
+    print("Generated password: ", generate_password(length))
+
+
+main()
